34a3c2fa015b889caf802a1a0f0e6b5df49c6ddf
Arjun Ramaswami <arjunr@mail.upb.de>
2019-07-14 12:37:48 +0200

    hostapi: fixed input data

--- host_api/main.c
+++ host_api/main.c
@@ -3,69 +3,79 @@
  *****************************************************************************/
 
 // global dependencies
+#define _POSIX_C_SOURCE 199309L
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-
-#ifdef DEBUG
 #include <fftw3.h>
-#endif
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
 
 // common dependencies
-#include "fpga/fft3d_decl.h"  // Common declarations
+#include "CL/opencl.h"
+#include "api/fft_fpga.h"  // Common declarations and API
 
 // local dependencies
-#include "fpga/fft3d_api.h"   // APIs 
 #include "common/argparse.h"  // Cmd-line Args to set some global vars
-#include "common/helper.h"    // helper functions
 
-// global variables
-static cmplx *fft_data, *fft_verify;
-unsigned int iter = 1;    // number of iterations of FFT3d using same input
-int N[3] = {64, 64, 64};  // Size of FFT3d
-bool inverse = false;     // Default as forward FFT
-char *fname = NULL;
+static void get_input_data(int N[3]);
+static void print_output(int N[3], double fftw_time, double fpga_runtime, int iter, char *fname);
+static void check_correctness_fftw(int N[3], cmplx *h_outData);
+static double compute_fftw(int N[3], int inverse);
+static void cleanup_fft();
+
+static cmplx *fft_data, *fft_data_out, *fft_verify;
+
+#ifdef __FPGA_SP
+  fftwf_plan plan;
+  fftwf_complex *fftw_data;
+#else
+  fftw_plan plan;
+  fftw_complex *fftw_data;
+#endif
 
 static const char *const usage[] = {
     "bin/host [options]",
     NULL,
 };
-
-#ifdef DEBUG
-  #if __FPGA_SP
-    fftwf_plan plan;
-    fftwf_complex *fftw_data;
-  #else
-    fftw_plan plan;
-    fftw_complex *fftw_data;
-  #endif
-
-  // function declarations for FFTW computation enabled using DEBUG flag on compilation
-  static double compute_fftw();
-  static void verify_fftw();
-#endif  //DEBUG
-
-// function declarations
-static void allocate_mem();
-static void get_input_data();
-static void cleanup();
-
-
-
-// --- CODE -------------------------------------------------------------------
-
+/******************************************************************************
+ * \brief  compute the offset in the matrix based on the indices of dim given
+ * \param  i, j, k - indices of different dimensions used to find the 
+ *         coordinate in the matrix 
+ * \param  N - fft size
+ * \retval linear offset in the flattened 3d matrix
+ *****************************************************************************/
+unsigned coord(unsigned N[3], unsigned i, unsigned j, unsigned k) {
+  // TODO : works only for uniform dims
+  return i * N[0] * N[1] + j * N[2] + k;
+}
+/******************************************************************************
+ * \brief  compute walltime in milliseconds
+ * \retval time in milliseconds
+ *****************************************************************************/
+double getTimeinMilliSec(){
+   struct timespec a;
+   clock_gettime(CLOCK_MONOTONIC, &a);
+   return (double)(a.tv_nsec) * 1.0e-6 + (double)(a.tv_sec) * 1.0e3;
+}
 int main(int argc, const char **argv) {
-  int N1 = 64, N2 = 64, N3 = 64;
-  double fpga_time = 0.0, fftw_time = 0.0;
+  unsigned i = 0;
+  double fpga_runtime = 0.0, fftw_time = 0.0;
+  char *data_path = "../fft3d_kernels/fpgabitstream";
 
-  // get cmd line params and save to global vars
+  // Cmd line argument declarations
+  int N[3] = {64, 64, 64};
+  unsigned iter = 1, inverse = 0;
+  char *fname = NULL;
+
   struct argparse_option options[] = {
     OPT_HELP(),
     OPT_GROUP("Basic Options"),
-    OPT_INTEGER('m',"n1", &N1, "FFT 1st Dim Size"),
-    OPT_INTEGER('n',"n2", &N2, "FFT 2nd Dim Size"),
-    OPT_INTEGER('p',"n3", &N3, "FFT 3rd Dim Size"),
+    OPT_INTEGER('m',"n1", &N[0], "FFT 1st Dim Size"),
+    OPT_INTEGER('n',"n2", &N[1], "FFT 2nd Dim Size"),
+    OPT_INTEGER('p',"n3", &N[2], "FFT 3rd Dim Size"),
     OPT_INTEGER('i',"iter", &iter, "Number of iterations"),
     OPT_BOOLEAN('b',"back", &inverse, "Backward/inverse FFT"),
     OPT_STRING('o',"output",&fname,"Output filename"), 
@@ -77,21 +87,27 @@ int main(int argc, const char **argv) {
   argparse_describe(&argparse, "Computing FFT3d using FPGA", "FFT size is mandatory, default number of iterations is 1");
   argc = argparse_parse(&argparse, argc, argv);
 
-  N[0] = N1;
-  N[1] = N2;
-  N[2] = N3;
+  printf("------------------------------\n");
+  printf("%s FFT3d Size : %d %d %d\n", inverse ? "Backward":"Forward", N[0], N[1], N[2]);
+  printf("------------------------------\n");
 
   // Allocate mem for input buffer and fftw buffer
-  printf("\nAllocating (%d, %d, %d) memory for FFT3d on the host ... \n\n", N[0],  N[1],  N[2]);
-  allocate_mem();
+  printf("Allocating Memory ...\n");
+  fft_data = (cmplx *)malloc(sizeof(cmplx) * N[0] * N[1] * N[2]);
+  fft_data_out = (cmplx *)malloc(sizeof(cmplx) * N[0] * N[1] * N[2]);
+  fft_verify = (cmplx *)malloc(sizeof(cmplx) * N[0] * N[1] * N[2]);
+  #if __FPGA_SP
+    fftw_data = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * N[0] * N[1] * N[2]);
+  #else
+    fftw_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N[0] * N[1] * N[2]);
+  #endif
 
   // Initialize input and produce verification data
-  printf("Getting input data ... \n\n");
-  get_input_data();
+  get_input_data(N);
 
   // execute FFT3d iter number of times
-  for( int i = 0; i < iter; i++){
-    printf("\n Iteration %d : Calculating %sFFT3d of size (%d, %d, %d) \n\n", i, inverse ? "inverse ":"", N[0], N[1], N[2]);
+  for( i = 0; i < iter; i++){
+    printf("\nCalculating %sFFT3d - %d\n", inverse ? "inverse ":"", i);
 
     // initialize FPGA
     if (fpga_initialize_()){
@@ -100,176 +116,174 @@ int main(int argc, const char **argv) {
     }
 
     // check if required bitstream exists
-    if(!fpga_check_bitstream_(N)){
+    if(!fpga_check_bitstream_(data_path, N)){
       printf("Bitstream not found\n");
     }
 
     // execute fpga fft3d
     double start = getTimeinMilliSec();
 #if __FPGA_SP
-    fpga_fft3d_sp_(0, "", inverse, fft_data);
+    fpga_fft3d_sp_(inverse, N, fft_data);
 #else
-    fpga_fft3d_dp_(0, "", inverse, fft_data);
+    fpga_fft3d_dp_(inverse, N, fft_data);
 #endif
     double stop = getTimeinMilliSec();
-    fpga_time += stop - start;
+    fpga_runtime += stop - start;
 
     // cleanup fpga
     printf("Cleanup up ... \n");
-    fpga_final_();
 
-#ifdef DEBUG
-    // Verify fpga fft3d with fftw fft3d
-    fftw_time+= compute_fftw();
-    verify_fftw();
-#endif
+    printf("Computing FFTW\n");
+    compute_fftw(N, inverse);
+    check_correctness_fftw(N, fft_data);
   }
 
   // Free the resources allocated
-  cleanup();
+  cleanup_fft();
 
   // Print performance metrics
-  print_output(fftw_time, fpga_time, iter, N, fname);
+  print_output(N, fftw_time, fpga_runtime, iter, fname);
 
   return 1;
 }
 
-/******************************************************************************
- * \brief  allocate memory for execution and verification
- *****************************************************************************/
-static void allocate_mem(){
-  fft_data = (cmplx *)malloc(sizeof(cmplx) * N[0] * N[1] * N[2]);
-  fft_verify = (cmplx *)malloc(sizeof(cmplx) * N[0] * N[1] * N[2]);
+static void get_input_data(int N[3]){
+  unsigned i = 0, j = 0, k = 0, where = 0;
+  const unsigned fname_len = 100;
+  char fname[fname_len];
 
-  
-#ifdef DEBUG
-  #if __FPGA_SP
-    fftw_data = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * N[0] * N[1] * N[2]);
-  #else
-    fftw_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N[0] * N[1] * N[2]);
-  #endif
-#endif
-
-}
-
- /******************************************************************************
- * \brief  create random values for FFT computation or read existing ones if 
- *         already saved in a file
- *****************************************************************************/
-static void get_input_data(){
-
-  char fname[50];
-  unsigned int where;
-
-  // TODO : Location of input and output files
   #if __FPGA_SP
     sprintf(fname,"../inputfiles/input_f%d_%d_%d.inp", N[0], N[1], N[2]);
   #else
     sprintf(fname,"../inputfiles/input_d%d_%d_%d.inp", N[0], N[1], N[2]);
   #endif
 
-  // if file already present, read from it
   FILE *fp = fopen(fname,"r");
   if(fp != NULL){
-      printf("Scanning data set from file %s ..\n\n",fname);
+      printf("Scanning data set from file - %s\n\n",fname);
 
-      for (unsigned int i = 0; i < N[0]; i++) {
-        for (unsigned int j = 0; j < N[1]; j++) {
-          for (unsigned int k = 0; k < N[2]; k++) {
-            where = coord(i, j, k, N);
+      for (i = 0; i < N[0]; i++) {
+        for (j = 0; j < N[1]; j++) {
+          for ( k = 0; k < N[2]; k++) {
+            where = coord(N, i, j, k);
 
-            #if __FPGA_SP
-              fscanf(fp, "%f %f ", &fft_data[where].x, &fft_data[where].y);
+            #ifdef __FPGA_SP
+            fscanf(fp, "%f %f ", &fft_data[where].x, &fft_data[where].y);
+            //printf("%f %f ", fft_data[where].x, fft_data[where].y);
             #else
-              fscanf(fp, "%lf %lf ", &fft_data[where].x, &fft_data[where].y);
+            fscanf(fp, "%lf %lf ", &fft_data[where].x, &fft_data[where].y);
             #endif
 
-#ifdef DEBUG
-            fftw_data[coord(i, j, k, N)][0] = fft_verify[coord(i, j, k, N)].x = fft_data[where].x;
-            fftw_data[coord(i, j, k, N)][1] = fft_verify[coord(i, j, k, N)].y = fft_data[where].y;
-#endif            
-            // TODO : debug to print to a file
+            fftw_data[where][0] = fft_verify[where].x = fft_data[where].x;
+            fftw_data[where][1] = fft_verify[where].y = fft_data[where].y;
             //printf(" %d %d %d - input[%d] = (%f, %f) fftw[%d] = (%f, %f) \n", i, j, k, where, fft_data[where].x, fft_data[where].y, where, fftw_data[where][0], fftw_data[where][1]);
-            //printf(" %d %d %d - input[%d] = (%lf, %lf) \n", i, j, k, where, fft_data[where].x, fft_data[where].y, where);
           }
         }
       }
-  } else{
-      printf("\tData not available. Printing random data set to file %s ..\n\n",fname);
+  }
+  else{
+      printf("Data not available. Printing random data set to file %s\n",fname);
 
-      // Create random data and print to a file to be reused 
       fp = fopen(fname,"w");
-      for (unsigned int i = 0; i < N[0]; i++) {
-        for (unsigned int j = 0; j < N[1]; j++) {
-          for (unsigned int k = 0; k < N[2]; k++) {
-            where = coord(i, j, k, N);
+      for (i = 0; i < N[0]; i++) {
+        for (j = 0; j < N[1]; j++) {
+          for ( k = 0; k < N[2]; k++) {
+            where = coord(N, i, j, k);
 
-#if __FPGA_SP 
+          #ifdef __FPGA_SP
             fft_data[where].x = (float)((float)rand() / (float)RAND_MAX);
             fft_data[where].y = (float)((float)rand() / (float)RAND_MAX);
             fprintf(fp, "%f %f ", fft_data[where].x, fft_data[where].y);
-#else
+          #else
             fft_data[where].x = (double)((double)rand() / (double)RAND_MAX);
             fft_data[where].y = (double)((double)rand() / (double)RAND_MAX);
             fprintf(fp, "%lf %lf ", fft_data[where].x, fft_data[where].y);
-#endif
-#ifdef DEBUG
-            fftw_data[coord(i, j, k, N)][0] = fft_verify[coord(i, j, k, N)].x = fft_data[where].x;
-            fftw_data[coord(i, j, k, N)][1] = fft_verify[coord(i, j, k, N)].y = fft_data[where].y;
-#endif
-            // TODO: Debug prints
-            // printf(" %d %d %d - input[%d] = (%f, %f) fftw[%d] = (%f, %f) \n", i, j, k, where, h_inData[where].x, h_inData[where].y, where, fftw_data[where][0], fftw_data[where][1]);
-            //printf(" %d %d %d - input[%d] = (%lf, %lf) \n", i, j, k, where, fft_data[where].x, fft_data[where].y, where);
+          #endif
+
+            fftw_data[where][0] = fft_verify[where].x = fft_data[where].x;
+            fftw_data[where][1] = fft_verify[where].y = fft_data[where].y;
+            printf(" %d %d %d - input[%d] = (%f, %f) fftw[%d] = (%f, %f) \n", i, j, k, where, fft_data[where].x, fft_data[where].y, where, fftw_data[where][0], fftw_data[where][1]);
           }
         }
       }
-
       fclose(fp);
   }
 }
 
-/******************************************************************************
- * \brief free memory allocated for computation and verification using FFTW
- *****************************************************************************/
-static void cleanup(){
-  if(fft_data)
-    free(fft_data);
+static void print_output(int N[3], double fftw_time, double fpga_runtime, int iter, char *fname){
+  char filename[] = "../../outputfiles/";
+  if(fname!= NULL){
+      strcat(filename, fname);
+  }
+  else{
+      printf("No output file entered\n");
+      strcat(filename, "noname.csv");
+  }
 
-  if(fft_verify)
-    free(fft_verify);
+/*
+  printf("Printing to %s\n", filename);
 
-#ifdef DEBUG
-  #if __FPGA_SP
-    fftwf_destroy_plan(plan);
-    fftwf_free(fftw_data);
-  #else
-    fftw_destroy_plan(plan);
-    fftw_free(fftw_data);
-   #endif
-#endif
+  FILE *fp = fopen(filename,"r");
+
+  if(fp == NULL){
+    fp = fopen(filename,"w");
+    fprintf(fp,"device,N,runtime,throughput\n");
+  }
+  else{
+    fp = fopen(filename,"a");
+  }
+
+  printf("\nNumber of runs: %d\n\n", iter);
+  printf("\tFFT Size\tRuntime(ms)\tThroughput(GFLOPS/sec)\t\n");
+  printf("fpga:");
+  fprintf(fp, "fpga,");
+
+  if(fpga_runtime != 0.0){
+    fpga_runtime = fpga_runtime / iter;
+    double gpoints_per_sec = ( N[0] * N[1] * N[2] / (fpga_runtime * 1E-6)) * 1E-9;
+    double gflops = 3 * 5 * N[0] * N[1] * N[2] * (log((double)N[0])/log((double)2))/(fpga_runtime * 1E-6 * 1E9);
+    printf("\t  %d³ \t\t %.4f \t  %.4f \t\n", N[0], (fpga_runtime * 1E-3), gflops);
+    fprintf(fp, "%d,%.4f,%.4f\n", N[0], (fpga_runtime * 1E-3), gflops);
+  }
+  else{
+    printf("Error in FFT3d \n");
+  }
+
+    printf("fftw:"); 
+    fprintf(fp, "fftw,"); 
+    if(fftw_time != 0.0){
+      fftw_time = fftw_time / iter;
+      double gpoints_per_sec = ( N[0] * N[1] * N[2] / (fftw_time * 1E-6)) * 1E-9;
+      double gflops = 3 * 5 * N[0] * N[1] * N[2]* (log((double)N[0])/log((double)2))/(fftw_time * 1E-6 * 1E9);
+      printf("\t  %d³ \t\t %.4f \t  %.4f \t\n", N[0], (fftw_time * 1E-3), gflops);
+      fprintf(fp, "%d,%.4f,%.4f\n", N[0], (fftw_time * 1E-3), gflops);
+    }
+    else{
+      printf("Error in FFT3d \n");
+    }
+
+  fclose(fp);
+  */
+
 }
-#ifdef DEBUG
-/******************************************************************************
- * \brief  compute FFTW 
- * \retval double : time taken to compute FFTW
- *****************************************************************************/
-static double compute_fftw(){
 
+static double compute_fftw(int N[3], int inverse){
 
 #if __FPGA_SP
-  printf("\nPlanning %ssingle precision FFTW ... \n", inverse ? "inverse ":"");
+  printf("\nPlanning %sSingle precision FFTW ... \n", inverse ? "inverse ":"");
   if(inverse){
       plan = fftwf_plan_dft_3d( N[0], N[1], N[2], &fftw_data[0], &fftw_data[0], FFTW_BACKWARD, FFTW_ESTIMATE);
   }
   else{
       plan = fftwf_plan_dft_3d( N[0], N[1], N[2], &fftw_data[0], &fftw_data[0], FFTW_FORWARD, FFTW_ESTIMATE);
   }
+  printf("Computing Single Precision FFTW\n");
   double start = getTimeinMilliSec();
   fftwf_execute(plan);
   double stop = getTimeinMilliSec();
 
 #else
-  printf("\nPlanning %sdouble precision FFTW ... \n", inverse ? "inverse ":"");
+  printf("\nPlanning %sDouble precision FFTW ... \n", inverse ? "inverse ":"");
   if(inverse){
       plan = fftw_plan_dft_3d( N[0], N[1], N[2], &fftw_data[0], &fftw_data[0], FFTW_BACKWARD, FFTW_ESTIMATE);
   }
@@ -277,8 +291,7 @@ static double compute_fftw(){
       plan = fftw_plan_dft_3d( N[0], N[1], N[2], &fftw_data[0], &fftw_data[0], FFTW_FORWARD, FFTW_ESTIMATE);
   }
 
-  printf("\nComputing FFTW ... \n\n");
-
+  printf("Computing Double Precision FFTW\n");
   double start = getTimeinMilliSec();
   fftw_execute(plan);
   double stop = getTimeinMilliSec();
@@ -287,21 +300,18 @@ static double compute_fftw(){
   return (stop - start);
 }
 
-/******************************************************************************
- * \brief  verify computed fft3d with FFTW fft3d
- *****************************************************************************/
-static void verify_fftw(){
-  unsigned int where;
+static void check_correctness_fftw(int N[3], cmplx *h_outData){
+  unsigned where, i = 0, j = 0, k = 0;
   #if __FPGA_SP
     float mag_sum = 0, noise_sum = 0, magnitude, noise;
   #else
     double mag_sum = 0, noise_sum = 0, magnitude, noise;
   #endif
 
-  for (unsigned int i = 0; i < N[0]; i++) {
-    for (unsigned int j = 0; j < N[1]; j++) {
-        for (unsigned int k = 0; k < N[2]; k++) {
-            where = coord(i, j, k, N);
+  for (i = 0; i < N[0]; i++) {
+    for (j = 0; j < N[1]; j++) {
+        for (k = 0; k < N[2]; k++) {
+            where = coord(N, i, j, k);
             // printf(" %d : %lf %lf - %lf %lf\n", where, h_outData[where].x, h_outData[where].y, fftw_data_out[where][0], fftw_data_out[where][1]);
             magnitude = fftw_data[where][0] - fft_data[where].x;
             noise = fftw_data[where][1] - fft_data[where].y;
@@ -313,7 +323,6 @@ static void verify_fftw(){
             decimal noise = (fftw_data[where][0] - fft_data[where].x) \
                 * (fftw_data[where][0] - fft_data[where].x) + 
                 (fftw_data[where][1] - fft_data[where].y) * (fftw_data[where][1] - fft_data[where].y);
-
             mag_sum += magnitude;
             noise_sum += noise;
 */
@@ -327,4 +336,22 @@ static void verify_fftw(){
   //noise_sum = noise_sum / (N * N * N);
   //printf("\t Average difference : %e, %e \n\n", mag_sum, noise_sum);
 }
-#endif
+
+static void cleanup_fft(){
+  if(fft_data)
+    free(fft_data);
+
+  if(fft_data_out)
+    free(fft_data_out);
+
+  if(fft_verify)
+    free(fft_verify);
+
+  #ifdef __FPGA_SP
+    fftwf_destroy_plan(plan);
+    fftwf_free(fftw_data);
+  #else
+    fftw_destroy_plan(plan);
+    fftw_free(fftw_data);
+  #endif
+}
